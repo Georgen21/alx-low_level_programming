ff13eaf77033249f1e189281a2fe58e1482d6595
Georgen21 <chukwudinwabia42@gmail.com>
2023-06-16 13:49:42 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,29 +2,27 @@
 
 /**
  * main - a program that prints all possible different combinations
- *
  * Return: 0
  */
 int main(void)
 {
 	int n, m;
 
-	for (n = 26; n <= 36; n++)
+	for (n = 48; n <= 56; n++)
 	{
-		for (m = 27; m = 37; m++)
+		for (m = 48; n <= 57; m++)
 		{
 			if (m > n)
 			{
 				putchar(n);
 				putchar(m);
-				if (n != 36 || m != 37)
+				if (n != 56 || m != 57)
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
 		}
-	}
 	putchar('\n');
 	return (0);
 }
