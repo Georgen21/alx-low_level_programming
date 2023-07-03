b1216576b4fb8d4de79c21b382ee77616e410ed9
Georgen21 <chukwudinwabia42@gmail.com>
2023-07-03 08:18:36 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,13 +6,16 @@
  * @b: char to copy
  * @n: number of times to copy b
  *
- * Return: Always 0.
+ * Return: Pointer to memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
+	{
 	s[i] = b;
+	}
+
 	return (s);
 }
